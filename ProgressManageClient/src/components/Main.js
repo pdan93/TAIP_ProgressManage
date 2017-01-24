a00e81440f4bd757826f15c90a323f55a9c634e7
Lulubul <didas_convict@yahoo.com>
2017-01-24 16:17:21 +0200

    changes

--- ProgressManageClient/src/components/Main.js
+++ ProgressManageClient/src/components/Main.js
@@ -2,13 +2,29 @@ require('normalize.css/normalize.css');
 require('styles/App.css');
 
 import React from 'react';
+import Rx from 'rx-react';
+import jQuery from 'jquery';
 
 class AppComponent extends React.Component {
   render() {
     return (
-      <div className="index">
-        <div className="notice">Hello World!</div>
-      </div>
+      <section className="index">
+          <article>
+            <select>
+              <option value="task1">task1</option>
+            </select>
+          </article>
+          <article>
+            <label id="demo">All states that went task:</label>
+            <ul id="revisionList"> </ul>
+
+            <label id="demo">All states in the petri nets:</label>
+            <ul id="stateList"> </ul>
+
+            <label> Petri nets evaluation message: </label>
+            <p id="PetriNets"> </p>
+          </article>
+      </section>
     );
   }
 }
@@ -17,3 +33,9 @@ AppComponent.defaultProps = {
 };
 
 export default AppComponent;
+/*
+  componentDidMount() {
+    var responseStream = Rx.Observable.fromPromise($.getJSON('localhost:59981/api/Revisions'));
+    console.log(responseStream);
+  }
+*/
